de1459709153a81070cbff0f0fa6f14c1f5b02ec
StephenJay123 <stephenjay290908@gmail.com>
2025-01-13 01:30:23 -0800

    Update Test.js

--- Test.js
+++ Test.js
@@ -216,7 +216,7 @@ function next(){
 
     while (true) {
         var answerposition1 = Math.round(Math.random() * 3)
-        if (answerposition != answerposition1) {
+        if (answerposition == answerposition1) {
             break
         }
     }
