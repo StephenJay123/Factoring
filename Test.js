80eb2e5efa44a31a3e353cf3d1bda42e6e402d94
StephenJay123 <stephenjay290908@gmail.com>
2025-01-15 15:58:47 -0800

    Update Test.js

--- Test.js
+++ Test.js
@@ -10,52 +10,49 @@ const ButtonThree = document.getElementById('ButtonThree');
 const ButtonFour = document.getElementById('ButtonFour');
 
 const questions = [
-    "x² + 5x + 6",       // Factorable as (x + 2)(x + 3)
-    "x² + 4x + 4",       // Factorable as (x + 2)(x + 2)
-    "x² + 7x + 10",      // Factorable as (x + 5)(x + 2)
-    "x² + 8x + 15",      // Factorable as (x + 3)(x + 5)
-    "x² + 3x + 2",       // Factorable as (x + 1)(x + 2)
-    "x² + 6x + 5",       // Factorable as (x + 1)(x + 5)
-    "x² + 2x - 3",       // Factorable as (x + 3)(x - 1)
-    "x² + 10x + 24",     // Factorable as (x + 4)(x + 6)
-    "x² + 9x + 20",      // Factorable as (x + 4)(x + 5)
-    "x² + 12x + 36",     // Factorable as (x + 6)(x + 6)
+    "x² + 5x + 6",       
+    "x² + 4x + 4",       
+    "x² + 7x + 10",      
+    "x² + 8x + 15",     
+    "x² + 3x + 2",     
+    "x² + 6x + 5",      
+    "x² + 2x - 3",     
+    "x² + 10x + 24",    
+    "x² + 9x + 20",     
+    "x² + 12x + 36",   
 
-    // Hard Trinomials
-    "2x² + 7x + 10",     // Factorable as (2x + 1)(x + 5)
-    "3x² + 8x + 4",      // Factorable as (3x + 4)(x + 1)
-    "4x² + 11x + 5",     // Factorable as (4x + 1)(x + 5)
-    "2x² + 9x + 7",      // Factorable as (2x + 7)(x + 1)
-    "3x² + 13x + 12",    // Factorable as (3x + 4)(x + 3)
-    "5x² + 19x + 12",    // Factorable as (5x + 4)(x + 3)
-    "6x² + 13x + 6",     // Factorable as (3x + 2)(2x + 3)
-    "5x² + 20x + 16",    // Factorable as (5x + 4)(x + 4)
-    "4x² + 15x + 9",     // Factorable as (4x + 3)(x + 3)
-    "2x² + 5x + 3",      // Factorable as (2x + 3)(x + 1)
+    "2x² + 11x + 5",    
+    "3x² + 7x + 4",    
+    "4x² + 21x + 5",    
+    "2x² + 9x + 7",     
+    "3x² + 13x + 12",  
+    "5x² + 19x + 12", 
+    "6x² + 13x + 6",    
+    "5x² + 24x + 16",   
+    "4x² + 15x + 9",   
+    "2x² + 5x + 3",     
 
-    // Difference of Squares
-    "x² - 9",            // Factorable as (x - 3)(x + 3)
-    "4x² - 25",          // Factorable as (2x - 5)(2x + 5)
-    "9x² - 16",          // Factorable as (3x - 4)(3x + 4)
-    "16x² - 81",         // Factorable as (4x - 9)(4x + 9)
-    "25x² - 49",         // Factorable as (5x - 7)(5x + 7)
-    "36x² - 64",         // Factorable as (6x - 8)(6x + 8)
-    "81x² - 1",          // Factorable as (9x - 1)(9x + 1)
-    "49x² - 121",        // Factorable as (7x - 11)(7x + 11)
-    "64x² - 100",        // Factorable as (8x - 10)(8x + 10)
-    "100x² - 81",        // Factorable as (10x - 9)(10x + 9)
+    "x² - 9",        
+    "4x² - 25",      
+    "9x² - 16",       
+    "16x² - 81",     
+    "25x² - 49",      
+    "36x² - 64",      
+    "81x² - 1",       
+    "49x² - 121",  
+    "64x² - 100",
+    "100x² - 81",  
 
-    // Perfect Square Trinomials
-    "x² + 6x + 9",       // Factorable as (x + 3)(x + 3)
-    "4x² + 12x + 9",     // Factorable as (2x + 3)(2x + 3)
-    "9x² + 24x + 16",    // Factorable as (3x + 4)(3x + 4)
-    "16x² + 24x + 9",    // Factorable as (4x + 3)(4x + 3)
-    "25x² + 30x + 9",    // Factorable as (5x + 3)(5x + 3)
-    "36x² + 72x + 36",   // Factorable as (6x + 6)(6x + 6)
-    "49x² + 42x + 9",    // Factorable as (7x + 3)(7x + 3)
-    "64x² + 64x + 16",   // Factorable as (8x + 4)(8x + 4)
-    "81x² + 72x + 16",   // Factorable as (9x + 4)(9x + 4)
-    "100x² + 80x + 16"   // Factorable as (10x + 4)(10x + 4)
+    "x² + 6x + 9",    
+    "4x² + 12x + 9",   
+    "9x² + 24x + 16", 
+    "16x² + 24x + 9",  
+    "25x² + 30x + 9",   
+    "36x² + 72x + 36", 
+    "49x² + 42x + 9",   
+    "64x² + 64x + 16",  
+    "81x² + 72x + 16",   
+    "100x² + 80x + 16"   
 ];
 
 const answers = [
@@ -69,6 +66,7 @@ const answers = [
     "(x + 4)(x + 6)",
     "(x + 4)(x + 5)",
     "(x + 6)²",
+    
     "(2x + 1)(x + 5)",
     "(3x + 4)(x + 1)",
     "(4x + 1)(x + 5)",
@@ -79,6 +77,7 @@ const answers = [
     "(5x + 4)(x + 4)",
     "(4x + 3)(x + 3)",
     "(2x + 3)(x + 1)",
+    
     "(x - 3)(x + 3)",
     "(2x - 5)(2x + 5)",
     "(3x - 4)(3x + 4)",
@@ -89,6 +88,7 @@ const answers = [
     "(7x - 11)(7x + 11)",
     "(8x - 10)(8x + 10)",
     "(10x - 9)(10x + 9)",
+    
     "(x + 3)²",
     "(2x + 3)²",
     "(3x + 4)²",
@@ -102,7 +102,6 @@ const answers = [
 ];
 
 const wrongAnswers = [
-    // Easy Trinomials
     ["(x + 1)(x + 6)", "(x + 3)²", "(x + 5)(x + 1)"],
     ["(x + 1)(x + 3)", "(x + 4)²", "(x + 3)(x + 5)"],
     ["(x + 7)(x + 1)", "(x + 4)(x + 3)", "(x + 8)(x + 1)"],
@@ -114,7 +113,6 @@ const wrongAnswers = [
     ["(x + 3)(x + 5)", "(x + 7)(x + 4)", "(x + 6)(x + 8)"],
     ["(x + 5)(x + 4)", "(x + 2)(x + 7)", "(x + 1)(x + 9)"],
 
-    // Hard Trinomials
     ["(2x + 2)(x + 6)", "(3x + 7)(x + 3)", "(2x + 5)(x + 4)"],
     ["(3x + 5)(x + 2)", "(2x + 3)(x + 4)", "(4x + 2)(x + 1)"],
     ["(2x + 4)(x + 3)", "(3x + 2)(x + 5)", "(5x + 3)(x + 6)"],
@@ -126,7 +124,6 @@ const wrongAnswers = [
     ["(6x + 4)(x + 5)", "(3x + 8)(x + 2)", "(5x + 7)(x + 6)"],
     ["(5x + 2)(x + 3)", "(3x + 9)(x + 5)", "(2x + 4)(x + 3)"],
 
-    // Difference of Squares
     ["(x - 3)(x + 4)", "(x - 2)(x + 5)", "(x - 4)(x + 7)"],
     ["(x - 4)(x + 5)", "(x - 2)(x + 6)", "(x - 5)(x + 4)"],
     ["(2x - 3)(x + 6)", "(x + 7)(x - 8)", "(2x - 1)(x + 3)"],
@@ -138,7 +135,6 @@ const wrongAnswers = [
     ["(5x + 4)(x + 7)", "(6x + 3)(x + 1)", "(2x + 3)(x + 5)"],
     ["(2x + 3)(x + 6)", "(4x + 7)(x + 4)", "(3x + 2)(x + 5)"],
 
-    // Perfect Square Trinomials
     ["(x - 3)²", "(x + 4)²", "(x - 2)²"],
     ["(x + 5)²", "(x - 6)²", "(x + 7)²"],
     ["(2x - 3)²", "(x + 6)²", "(x + 5)²"],
